eaaedcf0dbb966564a537acd79e1eeab3fe39bbc
jktemple <jktemple@ucsc.edu>
2023-03-17 12:42:21 -0700

    Fix

--- UntitledParkourGame/Assets/Scripts/InGameMenuBehaviors.cs
+++ UntitledParkourGame/Assets/Scripts/InGameMenuBehaviors.cs
@@ -77,8 +77,8 @@ public class InGameMenuBehaviors : MonoBehaviour
 
     public void QuitGame()
     {
-        SceneManager.LoadScene("Main Menu");
         NetworkManager.Singleton.Shutdown();
+        SceneManager.LoadScene("Main Menu");
         Debug.Log("Quit");
     }
 }
