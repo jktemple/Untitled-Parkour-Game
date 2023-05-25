b6605c0693345aeb9c472fddde1ab130c0a45d4d
jktemple <jktemple@ucsc.edu>
2023-05-25 16:29:33 -0700

    Rumble Test Scene

---
+++ UntitledParkourGame/Assets/Scripts/UI/RumbleTestUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static ControllerRumbleManager;
+
+public class RumbleTestUI : MonoBehaviour
+{
+    [Range(0.1f, 1.5f)]
+    public float PulseFrequency = 0.25f;
+
+    private float rumbleTime = 3f;
+    private RumblePattern rumblePattern = RumblePattern.Constant;
+    [SerializeField] Slider lowSlider;
+    [SerializeField] Slider lowSliderEnd;
+    [SerializeField] Slider highSlider;
+    [SerializeField] Slider highSliderEnd;
+    [SerializeField] Dropdown modeDropdown;
+    [SerializeField] ControllerRumbleManager rumbler;
+    private int[] timeDropdown = new int[] { 3, 5, 10 };
+    private RumblePattern[] rumbleMode = new RumblePattern[] { RumblePattern.Constant, RumblePattern.Pulse, RumblePattern.Linear };
+    public void SetDurration(int selectedValue)
+    {
+        rumbleTime = timeDropdown[selectedValue];
+    }
+
+    public void SetRumbleMode()
+    {
+        rumblePattern = rumbleMode[modeDropdown.value];
+    }
+
+    public void StartPressed()
+    {
+        switch (rumblePattern)
+        {
+            case RumblePattern.Constant:
+                rumbler.RumbleConstant(lowSlider.value, highSlider.value, rumbleTime);
+                break;
+            case RumblePattern.Pulse:
+                rumbler.RumblePulse(lowSlider.value, highSlider.value, PulseFrequency, rumbleTime);
+                break;
+            case RumblePattern.Linear:
+                rumbler.RumbleLinear(lowSlider.value, lowSliderEnd.value, highSlider.value, highSliderEnd.value, rumbleTime);
+                break;
+            default:
+                break;
+        }
+
+    }
+    public void StopPressed()
+    {
+        rumbler.StopRumble();
+    }
+}
