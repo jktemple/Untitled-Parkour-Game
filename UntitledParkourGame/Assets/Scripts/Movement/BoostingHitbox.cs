9b2310a12a80d726853a8c93e310f4c5508cca54
wgadd <115596867+wgadd@users.noreply.github.com>
2023-05-27 17:04:57 -0700

    successful boost test and updated, less confusing var name
    canBoost -> playerInHitbox

--- UntitledParkourGame/Assets/Scripts/Movement/BoostingHitbox.cs
+++ UntitledParkourGame/Assets/Scripts/Movement/BoostingHitbox.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class BoostingHitbox : NetworkBehaviour
 {
-    public bool canBoost;
+    public bool playerInHitbox;
 
     private void Start()
     {
-        canBoost = false;
+        playerInHitbox = false;
     }
 
     private void OnTriggerStay(Collider other)
@@ -17,8 +17,8 @@ public class BoostingHitbox : NetworkBehaviour
         if (!IsOwner) return;
         if (other.gameObject.GetComponentInChildren<PlayerMovement>() != null)
         {
-            canBoost = true;
-            Debug.Log("canBoost true");
+            playerInHitbox = true;
+            Debug.Log("playerInHitbox true");
         }
     }
 
@@ -27,8 +27,8 @@ public class BoostingHitbox : NetworkBehaviour
         if (!IsOwner) return;
         if (other.gameObject.GetComponentInChildren<PlayerMovement>() != null)
         {
-            canBoost = false;
-            Debug.Log("canBoost false");
+            playerInHitbox = false;
+            Debug.Log("playerInHitbox false");
         }
     }
 }
