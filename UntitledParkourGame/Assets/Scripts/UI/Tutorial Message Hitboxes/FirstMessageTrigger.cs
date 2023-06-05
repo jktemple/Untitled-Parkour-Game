e85ff3e2c65abba84b4076d7fb4a916dd1002f84
wgadd <115596867+wgadd@users.noreply.github.com>
2023-06-04 20:17:45 -0700

    set up all triggers with correct text, but still need to have their shape and pos adjusted and the in game text removed. also geometry fixes

--- UntitledParkourGame/Assets/Scripts/UI/Tutorial Message Hitboxes/FirstMessageTrigger.cs
+++ UntitledParkourGame/Assets/Scripts/UI/Tutorial Message Hitboxes/FirstMessageTrigger.cs
@@ -6,10 +6,69 @@ public class FirstMessageTrigger : MonoBehaviour
 {
 
     public TutorialMessages TutorialMessage;
+    private string newMessage;
+
+    private void Start()
+    {
+        newMessage = "unset";
+        if(this.gameObject.name == "Message")
+        {
+            newMessage = "Welcome!\nTo move, use the left stick / WASD keys";
+        }
+        else if(this.gameObject.name == "Message (1)")
+        {
+            newMessage = "Oh boy, a gap!\nJump over it!\n(RB / R1 / R / Spacebar)";
+        }
+        else if (this.gameObject.name == "Message (2)")
+        {
+            newMessage = "Bigger Gap means More Speed!\nSprint!(LT / L2 / ZL / LeftShift)";
+        }
+        else if (this.gameObject.name == "Message (3)")
+        {
+            newMessage = "Watch Your Head!\nDon't worry, you can just walk under this one.";
+        }
+        else if (this.gameObject.name == "Message (4)")
+        {
+            newMessage = "Rather small, huh?\nJust slide!\n(LB / L1 / L / LeftCtrl while moving) ";
+        }
+        else if (this.gameObject.name == "Message (5)")
+        {
+            newMessage = "To run across a wall, just move into it after a jump.";
+        }
+        else if (this.gameObject.name == "Message (6)")
+        {
+            newMessage = "You can chain wallruns into each other by jumping across!";
+        }
+        else if (this.gameObject.name == "Message (7)")
+        {
+            newMessage = "Climbing up the wall is simple: just move/jump into the wall and move ahead.";
+        }
+        else if (this.gameObject.name == "Message (8)")
+        {
+            newMessage = "You can do a Quickturn!\n(Click Right Stick or Q)\nCareful, though: for best results, don't move when jumping off walls!";
+        }
+        else if (this.gameObject.name == "Message (9)")
+        {
+            newMessage = "You can even use the Quickturn to chain a wallrun into a wallclimb!";
+        }
+        else if (this.gameObject.name == "Message (10)")
+        {
+            newMessage = "Congrats on finishing the tutorial course!";
+        }
+        else
+        {
+            newMessage = "...";
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        TutorialMessage.changeMessage("FIRST MESSAGE");
+        TutorialMessage.changeMessage(newMessage);
         TutorialMessage.showMessage();
     }
 
