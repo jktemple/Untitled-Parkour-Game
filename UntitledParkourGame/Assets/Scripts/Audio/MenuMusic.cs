3aacf60084d4039a5d8411845312f98d64ef5698
jktemple <jktemple@ucsc.edu>
2023-06-08 23:28:44 -0700

    Music Updates

--- UntitledParkourGame/Assets/Scripts/Audio/MenuMusic.cs
+++ UntitledParkourGame/Assets/Scripts/Audio/MenuMusic.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuMusic : MonoBehaviour
 {
     // Start is called before the first frame update
+    Scene tutorial;
+    Scene bigmap;
+    Scene smallmap;
+    Scene texturedBigMap;
+    Scene texturedSmallMap;
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
+        tutorial = SceneManager.GetSceneByName("New Tutorial Level");
+        bigmap = SceneManager.GetSceneByName("LobbyMap");
+        smallmap = SceneManager.GetSceneByName("SmallMap");
+        texturedBigMap = SceneManager.GetSceneByName("BigConTextured");
+        texturedSmallMap = SceneManager.GetSceneByName("SmallConTextured");
+
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    
 }
